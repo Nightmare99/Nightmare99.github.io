39da6739f808a966cc8c449ce8f9eb53084f7836
Vishal <vishal.s.kumar99@gmail.com>
2025-10-27 03:30:35 +0530

    feat: add GitHub Pages deployment workflow and update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // GitHub Pages will serve from root since repo is Nightmare99.github.io
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
